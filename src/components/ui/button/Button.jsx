c23bc4104ce95fc96a34eabacead8697a872506c
musienko <musienko@grechka.digital>
2025-05-01 16:48:01 +0300

    in process

--- src/components/ui/button/Button.jsx
+++ src/components/ui/button/Button.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import styles from './styles.module.scss';
-import {cn} from '@/lib/utils.js';
+import { cn } from '@/lib/utils.js';
 
-export const Button = ({title, ...props}) => {
+export const Button = ({ title, ...props }) => {
   return (
-    <button className={cn(styles.button, props.active && styles.buttonActive)} {...props}>{title}
-    <div className={cn(styles.underline, props.active && styles.underlineActive)}></div>
+    <button className={cn(styles.button, props.active && styles.buttonActive)} {...props}>
+      {title}
+      <div className={cn(styles.underline, props.active && styles.underlineActive)}></div>
     </button>
-  )
-}
+  );
+};
